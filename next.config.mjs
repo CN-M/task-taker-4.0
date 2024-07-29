9d853ece6dc80d8e0e6780629836b5696211799b
C.N. <cn.mbhalati@gmail.com>
2024-07-29 14:39:12 +0200

    Edit next config so @node-rs/argon2 doesn't get bundled

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   serverComponentsExternalPackages: ["@node-rs/argon2"],
-  // },
-  webpack: (config) => {
-    config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
-    return config;
+  experimental: {
+    serverComponentsExternalPackages: ["@node-rs/argon2"],
   },
 };
 
