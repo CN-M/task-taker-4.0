70aa90e5f7a9e90db9326c0eb2850343f28aa716
C.N. <cn.mbhalati@gmail.com>
2024-07-29 14:46:55 +0200

    remove hash and verify utilities from lib/utils so proper building can occur

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-import { hash as hashArgon2, verify as verifyArgon2 } from "@node-rs/argon2";
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -11,23 +9,4 @@ const defaultOptions = {
   timeCost: 2,
   outputLen: 32,
   parallelism: 1,
-};
-
-export const hash = async (
-  password: string,
-  options: {
-    memoryCost?: number;
-    timeCost?: number;
-    outputLen?: number;
-    parallelism?: number;
-  }
-): Promise<string> => {
-  return await hashArgon2(password, options);
-};
-
-export const verify = async (
-  password: string,
-  otherPassword: string
-): Promise<boolean> => {
-  return await verifyArgon2(password, otherPassword);
 };
